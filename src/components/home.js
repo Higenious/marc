5f13fb61336f45ebe0f126a14c6b92e5b9cd4974
cchetantayade@gmail.com <cchetantayade@gmail.com>
2020-03-02 23:52:32 +0530

    first commit

---
+++ src/components/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class home extends Component {
+    render() {
+        return (
+            <div>
+                 <h1>Welcome to online exam dashboard !</h1>
+            </div>
+        );
+    }
+}
+
+export default home;
